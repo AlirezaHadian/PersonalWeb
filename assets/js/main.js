2502cdc206f9af4ac88c8a165d0b313d116aacd1
Alireza Hadian <ali.h.reza8@gmail.com>
2023-02-26 12:49:24 +0330

    Portfolio modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,18 +72,59 @@ modalCloses.forEach((modalClose)=> {
   })
 })
 /*==================== PORTFOLIO SWIPER  ====================*/
-    var swiper = new Swiper('.swiper', {
-      cssMode: true,
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      }
-    });
+    // var swiper = new Swiper('.swiper', {
+    //   cssMode: true,
+    //   loop: true,
+    //   navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    //   },
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    //   }
+    // });
+
+/*==================== PORTFOLIO MODAL ====================*/
+function openModal() {
+  document.getElementById("portfolioModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("portfolioModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
 
 /*==================== TESTIMONIAL ====================*/
 
